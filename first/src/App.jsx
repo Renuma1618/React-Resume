d379c782bbe3fcea3bfecd9efb268d517b69e0a3
Renuma1618 <renuka1682003@gmail.com>
2025-04-05 23:36:03 +0530

    reactapp

---
+++ first/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RenukaProfile from "./RenukaProfile";
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen p-4">
+      <RenukaProfile />
+    </div>
+  );
+}
+
+export default App;
